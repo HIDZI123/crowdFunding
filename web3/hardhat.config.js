43df48234120876f620aa5d37e645f0e2fb776fa
Mustafa <yusufmustufa@gmail.com>
2025-02-22 16:26:06 +0530

    Gettig started with the react application

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,10 +1,9 @@
-require("@matterlabs/hardhat-zksync-solc");
-require("@matterlabs/hardhat-zksync-verify");
-
+/* require("@matterlabs/hardhat-zksync-solc");
+require("@matterlabs/hardhat-zksync-verify"); */
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
+ /*  zksolc: {
     version: "1.4.1",
     compilerSource: "binary",
     settings: {
@@ -36,9 +35,17 @@ module.exports = {
     cache: "./cache-zk",
     sources: "./contracts",
     tests: "./test",
-  },
+  }, */
   solidity: {
     version: "0.8.23",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
